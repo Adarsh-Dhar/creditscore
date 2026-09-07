e65f706fc05bf096ffa49dbd3348ea5d49fbf270
Adarsh Dhar <dharadarsh0@gmail.com>
2026-09-07 11:46:27 +0530

    db setup to prisma v8

--- indexer/prisma/migrations/20260819052851_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "IndexedEvent" ADD COLUMN     "protocol" TEXT NOT NULL DEFAULT 'aave';
-
--- CreateIndex
-CREATE INDEX "IndexedEvent_protocol_idx" ON "IndexedEvent"("protocol");
