db6da16a5cfde01a6f7c90847d0c112b65f3ea61
Adarsh Dhar <dharadarsh0@gmail.com>
2026-09-07 20:21:24 +0530

    cors errors

--- db/src/prisma/db.ts
+++ db/src/prisma/db.ts
@@ -1,11 +1,15 @@
 import 'dotenv/config';
+import { Temporal } from '@js-temporal/polyfill';
+
+if (!(globalThis as any).Temporal) {
+  (globalThis as any).Temporal = Temporal;
+}
+
 import postgres from '@prisma/orm-postgres/runtime';
 import type { Contract } from './contract.js';
 import contractJson from './contract.json' with { type: 'json' };
 
-const client = postgres<Contract>({
+export const db = postgres<Contract>({
   contractJson,
   url: process.env.DATABASE_URL!,
 });
-
-export const db = await client.connect();
